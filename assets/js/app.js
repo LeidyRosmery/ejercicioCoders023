1ebc0e8b6489b5352d4c025aaba8e9b23c42e0a0
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-19 13:20:52 -0500

    primeras coders

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,17 +19,7 @@ div01.appendChild(coder01);
 div02.appendChild(coder02);
 div03.appendChild(coder03);
 /*****************************************************/
-var seccSegundo=document.createElement("section");
-seccSegundo.setAttribute("id","imagenes1");
-var div04=document.createElement("div");
-var coder04=document.createElement("img");
-coder04.setAttribute("src","assets/img/14.png");
-var div05=document.createElement("div");
-var coder05=document.createElement("img");
-coder05.setAttribute("src","assets/img/15.png");
-var div06=document.createElement("div");
-var coder06=document.createElement("img");
-coder06.setAttribute("src","assets/img/16.png");
+
 
 /******************************************************/
 seccPrimero.appendChild(div01);
@@ -40,12 +30,3 @@ seccEncabezado.appendChild(barra);
 document.body.appendChild(seccEncabezado);
 document.body.appendChild(seccPrimero);
 var seccSegundo=document.createElement("section");
-
-
-div04.appendChild(coder04);
-div05.appendChild(coder05);
-div06.appendChild(coder06);
-seccSegundo.appendChild(div04);
-seccSegundo.appendChild(div05);
-seccSegundo.appendChild(div06);
-document.body.appendChild(seccSegundo);
