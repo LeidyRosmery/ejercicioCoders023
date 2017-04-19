5fba73fefa030a87d453564f51869846ea717640
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-19 13:40:38 -0500

    completo 6 imagenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,18 +15,33 @@ coder02.setAttribute("src","assets/img/12.png");
 var div03=document.createElement("div");
 var coder03=document.createElement("img");
 coder03.setAttribute("src","assets/img/13.png");
+var div04=document.createElement("div");
+var coder04=document.createElement("img");
+coder04.setAttribute("src","assets/img/14.png");
+var div05=document.createElement("div");
+var coder05=document.createElement("img");
+coder05.setAttribute("src","assets/img/15.png");
+var div06=document.createElement("div");
+var coder06=document.createElement("img");
+coder06.setAttribute("src","assets/img/16.png");
 div01.appendChild(coder01);
 div02.appendChild(coder02);
 div03.appendChild(coder03);
+div04.appendChild(coder04);
+div05.appendChild(coder05);
+div06.appendChild(coder06);
 /*****************************************************/
 
 
 /******************************************************/
+
 seccPrimero.appendChild(div01);
 seccPrimero.appendChild(div02);
 seccPrimero.appendChild(div03);
+seccPrimero.appendChild(div04);
+seccPrimero.appendChild(div05);
+seccPrimero.appendChild(div06);
 seccEncabezado.appendChild(parrafo);
 seccEncabezado.appendChild(barra);
 document.body.appendChild(seccEncabezado);
 document.body.appendChild(seccPrimero);
-var seccSegundo=document.createElement("section");
